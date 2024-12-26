9e8cadc619f5adf2b3b4d35446c0cded9d89be35
DIMONZ23 <ducthang28122003>
2024-12-26 15:17:10 +0700

    [Dec-W04][UT] Create E2E Cypress

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: 'localhost', // hoặc '0.0.0.0' nếu muốn truy cập từ mạng LAN
+    port: 3000,        // Đổi cổng từ mặc định (5173) sang 3000
+    open: true         // Tự động mở trình duyệt khi chạy
   }
 })
